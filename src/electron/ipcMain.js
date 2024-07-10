7517851c39f83c91874abfdfe31a1c2a30a2c6f2
Weexy <zhengweixin0101@outlook.com>
2024-07-10 10:58:44 +0800

    更新 ipcMain.js

--- src/electron/ipcMain.js
+++ src/electron/ipcMain.js
@@ -253,7 +253,7 @@ export function initIpcMain(win, store, trayEventEmitter) {
       details: track.name + ' - ' + track.ar.map(ar => ar.name).join(','),
       state: track.al.name,
       largeImageKey: 'logo',
-      largeImageText: 'YesPlayMusic',
+      largeImageText: 'Music',
       smallImageKey: 'pause',
       smallImageText: 'Pause',
       instance: true,
